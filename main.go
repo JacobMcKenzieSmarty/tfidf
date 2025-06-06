9d547f3f458e36e711f220bc03761eac4d9f92f0
Jacob McKenzie <jacob.mckenzie@smarty.com>
2025-06-06 08:26:11 -0600

    Update main.go to reflect the presentation

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	// Step 1: Input
+	// Document Corpus
 	docs := []model.Document{
 		//{0, "apple orange banana"},
 		//{1, "banana apple"},
@@ -18,21 +18,22 @@ func main() {
 		{2, "3845 W 5400 S SANDY"},
 	}
 
+	//Step 1: Preprocessing Data
 	vocab, docTFs, df := pipeline.BuildVocabAndTF(docs)
 	idf := pipeline.ComputeIDF(df, len(docs))
-
-	// Step 2: Document vectors
 	docVecs := pipeline.BuildTFIDFVectors(docTFs, idf)
+
+	//Step 2: Indexing (Inverted-Index)
 	invertedIndex := pipeline.MakeInvertedIndex(docTFs)
 
-	// Step 3: Query vector
+	// Step 3: Query Processing
 	query := "3845 S"
 	queryVec, candidates := pipeline.BuildQueryTFIDFVector(query, vocab, idf, invertedIndex)
 
-	// Step 4: Score
+	// Step 4: Scoring
 	scores := pipeline.ScoreDocuments(queryVec, docVecs, candidates)
 
-	// Step 5: Print
+	// Step 5: Returning Results
 	for i, score := range scores {
 		fmt.Printf("Rank %d: Doc %d (score: %.4f)\n", i+1, score.DocID, score.Value)
 	}
